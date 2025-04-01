b5a3983b0da14996b1c921fb10472a437b14456c
Mariano Martinez Grasso <marianomartinez84@gmail.com>
2025-04-01 11:46:42 -0300

    Implement RunWorkflow command

--- src/ecoscope_eda_core/messages/commands.py
+++ src/ecoscope_eda_core/messages/commands.py
@@ -1,10 +1,25 @@
+from enum import Enum
+from typing import Dict, Any
+
 from pydantic import BaseModel, Field
 from .core import Command
 
 
+class InvokerType(str, Enum):
+    BLOCKING_SUBPROCESS = "BlockingLocalSubprocessInvoker"
+    K8S_JOB = "K8sJobInvoker"
+
+
 class RunWorkflowParams(BaseModel):
-    # ToDo: Add parameters
-    pass
+    conda_channel: str = Field(..., title="Conda channel")
+    conda_package: str = Field(..., title="Package name")
+    invoker_type: InvokerType = Field(
+        InvokerType.BLOCKING_SUBPROCESS, title="Invoker type"
+    )
+    invoker_kwargs: Dict[str, Any] = Field(
+        default_factory=dict, title="Invoker keyword arguments"
+    )
+    command: str = Field(..., title="Command to run")
 
 
 class RunWorkflow(Command):
